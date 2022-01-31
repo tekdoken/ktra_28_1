f4c4eba97b7444eefaa74b9457bc1756cc6193ac
tekdoken <tekdokenzx@gmail.com>
2022-01-31 18:18:52 +0700

    list create detail delete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import {HttpClientModule} from "@angular/common/http";
 import { BooksListComponent } from './book/books-list/books-list.component';
 import { BookCreateComponent } from './book/book-create/book-create.component';
 import { BookDetailComponent } from './book/book-detail/book-detail.component';
+import { BookEditComponent } from './book/book-edit/book-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BooksListComponent,
     BookCreateComponent,
-    BookDetailComponent
+    BookDetailComponent,
+    BookEditComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/book/book-detail/book-detail.component.ts
@@ -0,0 +1,31 @@
+import {Component, OnInit} from '@angular/core';
+import {BookService} from "../../service/book.service";
+import {ActivatedRoute, Router} from "@angular/router";
+import {Book} from "../../model/book";
+
+@Component({
+  selector: 'app-book-detail',
+  templateUrl: './book-detail.component.html',
+  styleUrls: ['./book-detail.component.css']
+})
+export class BookDetailComponent implements OnInit {
+  book: Book = {}
+
+  constructor(private service: BookService, private activatedRoute: ActivatedRoute, private router: Router) {
+  }
+
+  ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe(paramap => {
+      const id = paramap.get('id');
+      this.service.getOne(id).subscribe(res => {
+        this.book = res;
+      })
+    })
+  }
+
+  remote(id: any) {
+    this.service.delete(id).subscribe(r=>{
+      this.router.navigate([''])
+    })
+  }
+}
