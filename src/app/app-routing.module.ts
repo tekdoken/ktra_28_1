a32f33befedf410c3de7e9ca126428be1e14d30d
tekdoken <tekdokenzx@gmail.com>
2022-01-31 16:28:44 +0700

    list create

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {BooksListComponent} from "./book/books-list/books-list.component";
+import {BookCreateComponent} from "./book/book-create/book-create.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: BooksListComponent
+    // , children: [{
+    //   path: 'create',
+    //   component: BookCreateComponent
+    // }]
+  },  {
+    path: 'create',
+    component: BookCreateComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import { BooksListComponent } from './book/books-list/books-list.component';
+import { BookCreateComponent } from './book/book-create/book-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BooksListComponent,
+    BookCreateComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
